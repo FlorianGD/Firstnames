c13d714121e10bf753821ce64bde2d22403380e6
Florian Gaudin-Delrieu <florian.gaudindelrieu@gmail.com>
2016-04-26 14:27:56 +0200

    Change graph title to change on click only

--- server.R
+++ server.R
@@ -19,7 +19,10 @@ shinyServer(function(input, output, session) {
   dataPrenom<-eventReactive(input$action,{
     queryStream(input$prenom)
   })
- 
+  
+  prenom<-eventReactive(input$action,{
+    input$prenom
+  })
   output$text1<-renderText({
     paste0("Nombre de ligne : ",nrow(dataPrenom()),
            "\nClasse : ",class(dataPrenom()))
@@ -34,10 +37,11 @@ shinyServer(function(input, output, session) {
     head(dataPrenom(), n = input$numHead)
   })
   
+  
   output$prenomMetiers<-renderPlotly({
     ggplotly(ggplot(dataPrenom() %>% distinct(item),aes(x=annee))+
       geom_histogram(binwidth = 10,aes(fill=pays))+
-      ggtitle(paste("Répartition des", input$prenom ,"dans Wikidata"))+
+      ggtitle(paste("Répartition des", prenom() ,"dans Wikidata"))+
       ylab("Nombre")+
       xlab("Année de naissance") +
       scale_x_continuous(limits=c(input$dates[1],input$dates[2]))+
